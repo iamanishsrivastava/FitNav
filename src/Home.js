dd1c7ea9f6e4fe704867627252abb0a179df2239
Anish Srivastava <anishamsri@gmail.com>
2023-08-17 16:39:34 +0530

    Created chat app at bottom-right (Home)

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'; // Import your Home section CSS file
 
 function Home() {
+  
+  const [isChatOpen, setIsChatOpen] = useState(false); // State to track chat window status
+  const [searchQuery, setSearchQuery] = useState('');
+  const [chatMessages, setChatMessages] = useState([]); // State to track chat messages
+
+  // Function to handle search and expand chat
+  const handleSearch = () => {
+    setIsChatOpen(true);
+    if (searchQuery.trim() !== '') {
+      // Add user's message to chat messages
+      setChatMessages((prevMessages) => [
+        ...prevMessages,
+        { sender: 'user', text: searchQuery },
+      ]);
+      setSearchQuery('');
+    }
+  };
+
+  // Function to handle chat input change
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <section className="home">
       <div className="home-content">
         <div className="left-section">
         {
             /* workout images and AI chat bot*/
-            
+            <a href="https://unsplash.com/photos/xXofYCc3hqc">
+              <img src="https://unsplash.com/photos/xXofYCc3hqc" />
+            </a>
         }
         </div>
-        <div className="right-section">
+        <div className={`right-section ${isChatOpen ? 'open' : ''}`}>
           <div className="chat-container">
-            {
-                /* Display chat messages here */
-                
-            }
-          </div>
-          <div className="input-container">
-            <input type="text" placeholder="Ask a question" />
-            <button>Send</button>
+            {chatMessages.map((message, index) => (
+              <div
+                className={`chat-message ${message.sender}`}
+                key={index}
+              >
+                <span className="message-sender">
+                  {message.sender === 'fitnav' ? 'FitNav' : 'You'}
+                </span>
+                <p className="message-text">{message.text}</p>
+              </div>
+            ))}
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Type your question..."
+                value={searchQuery}
+                onChange={handleInputChange}
+              />
+              <button onClick={handleSearch}>Send</button>
+            </div>
           </div>
         </div>
       </div>
