27a8d56e73baf72257ef15bdbc6c6ff69464d7ea
Aditya Kumar <116816791+imadityakr@users.noreply.github.com>
2023-08-17 19:00:18 +0530

    Create Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <ul>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#test">Testimonials</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#home">Home</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
