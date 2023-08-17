f0b97a0a9fd726d33f4dae90d0cefcfea27d0635
Anish Srivastava <anishamsri@gmail.com>
2023-08-18 03:59:53 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar.js';
+
+import React from 'react';
 import Home from './Home.js';
+import Navbar from './Navbar.js';
+import Services from './Services.js';
 import About from './About.js';
-import Services from './Services.js'; 
 
 function App() {
-  const [selectedSection, setSelectedSection] = useState('home');
-
-  
-  const handleSectionChange = (section) => {
-    setSelectedSection(section);
-  };
-
-  let sectionToRender;
-  if (selectedSection === 'home') {
-    sectionToRender = <Home />;
-  } else if (selectedSection === 'about') {
-    sectionToRender = <About />;
-  } else if (selectedSection === 'services') {
-    sectionToRender = <Services />;
-  }
-
   return (
     <div className="App">
-      <Navbar onSectionChange={handleSectionChange} />
-      {sectionToRender}
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,11 +9,11 @@ const Navbar = () => {
     <header>
       <nav className="navbar">
         <ul>          
-          <li><a href="./Home.js">Home</a></li>
-          <li><a href="./About.js">About</a></li>
-          <li><a href="./Services.js">Services</a></li>
-          <li><a href="./Testimonial.js">Testimonials</a></li>
-          <li><a href="./Contact.js">Contact</a></li>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#About">About</a></li>
+          <li><a href="#Services">Services</a></li>
+          <li><a href="#Testimonial">Testimonials</a></li>
+          <li><a href="#Contact">Contact</a></li>
         </ul>
       </nav>
     </header>
