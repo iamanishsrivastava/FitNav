f2611d9806e89f79ac5434141af255ce566aacb8
Anish Srivastava <anishamsri@gmail.com>
2023-08-17 19:54:54 +0530

    Basic Frontend with nav, home, chat app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 import React from 'react';
 import Home from './Home.js';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,99 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import './Home.css'; // Import your Home section CSS file
+import './Home.css';
 
 function Home() {
-  const [isChatOpen, setIsChatOpen] = useState(false); // State to track chat window status
+  const [isChatOpen, setIsChatOpen] = useState(true); // Always open by default
   const [searchQuery, setSearchQuery] = useState('');
-  const [chatMessages, setChatMessages] = useState([]); // State to track chat messages
+  const [chatMessages, setChatMessages] = useState([
+    // Initial message from FitNav
+    { sender: 'FitNav', text: ' Hello! How can I help?' },
+  ]);
 
-  // Function to handle search and expand chat
   const handleSearch = () => {
-    setIsChatOpen(true);
     if (searchQuery.trim() !== '') {
-      // Add user's message to chat messages
       setChatMessages((prevMessages) => [
         ...prevMessages,
-        { sender: 'You', text: searchQuery }, // Sender is 'You'
+        { sender: 'You', text: searchQuery },
       ]);
       setSearchQuery('');
     }
   };
 
-  // Function to handle chat input change
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  const [currentImage, setCurrentImage] = useState(0);
-
-  // Function to handle image transition
-  const handleImageTransition = () => {
-    setCurrentImage((prevImage) => (prevImage + 1) % 3);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(handleImageTransition, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    
     <section className="home">
-      <div className="home-content">
       <div className="left-section">
-          <div className="image-stack">
-            <img
-              src="https://source.unsplash.com/180x260/?vector-fitness"
-              alt="Workout"
-              className={`image ${currentImage === 0 ? 'active' : ''}`}
-            />
-            <img
-              src="https://source.unsplash.com/180x260/?vector-fitness"
-              alt="Workout"
-              className={`image ${currentImage === 1 ? 'active' : ''}`}
-            />
-            <img
-              src="https://source.unsplash.com/180x260/?fitness"
-              alt="Workout"
-              className={`image ${currentImage === 2 ? 'active' : ''}`}
-            />
-          </div>
-        </div>
-        <div className={`right-section ${isChatOpen ? 'open' : ''}`}>
-          <div className="chat-container">
-            {chatMessages.map((message, index) => (
-              <div
-                className={`chat-message ${message.sender}`}
-                key={index}
-              >
-                <span className="message-sender">
-                  {message.sender === 'fitnav' ? 'FitNav' : 'You'}
-                </span>
-                <p className="message-text">{message.text}</p>
-              </div>
-            ))}
-            <div className="search-bar">
-              <input
-                type="text"
-                placeholder="Type your question..."
-                value={searchQuery}
-                onChange={handleInputChange}
-                onKeyPress={(e) => {
-                  if (e.key === 'Enter') {
-                    handleSearch();
-                  }
-                }}
-              />
-              <button onClick={handleSearch}>Send</button>
-            </div>
-          </div>
+        <div className="fitnav-info">
+          <h2>FitNav</h2>
+          <p>Your Personal Fitness Companion</p>
         </div>
       </div>
-      <div className="section-links">
-        <a href="About.js">About</a>
-        <a href="Services.js">Services</a>
-        <a href="Testimonials.js">Testimonials</a>
-        <a href="Contact.js">Contact Us</a>
+      <div className={`right-section ${isChatOpen ? 'open' : ''}`}>
+        <div className="chat-container">
+          {chatMessages.map((message, index) => (
+            <div className={`chat-message ${message.sender}`} key={index}>
+              <span className="message-sender">
+                {message.sender === 'FitNav' ? 'FitNav: ' : 'You: '}
+              </span>
+              <p className="message-text">{message.text}</p>
+            </div>
+          ))}
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Type your question..."
+              value={searchQuery}
+              onChange={handleInputChange}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  handleSearch();
+                }
+              }}
+            />
+            <button onClick={handleSearch}>Send</button>
+          </div>
+        </div>
       </div>
     </section>
   );
