3ed713cac01fd25fafba9b84fcb374b4f28ca161
Adarsh <starcoder17@gmail.com>
2023-08-18 05:50:25 +0530

    working macha

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import React from 'react';
 import Home from './Home.js';
 import Navbar from './Navbar.js';
-// import Services from './Services.js';
-// import About from './About.js';
+import Services from './Services.js';
+import About from './About.js';
+import Contact from './Contact.js';
+import Testimonials from './Testimonials.js';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <Home />
-      {/* <About />
-      <Services /> */}
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/services" element={<Services/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/testimonials" element={<Testimonials/>}/>
+      </Routes>
     </div>
+    </Router>
+    
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 
@@ -9,11 +10,11 @@ const Navbar = () => {
     <header>
       <nav className="navbar">
         <ul>          
-          <li><a href="#Home">Home</a></li>
-          <li><a href="#About">About</a></li>
-          <li><a href="#Services">Services</a></li>
-          <li><a href="#Testimonial">Testimonials</a></li>
-          <li><a href="#Contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/testimonials">Testimonials</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
